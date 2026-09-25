3d46c83a725d6d3b912042cab53bc36fb1813b7e
abdollahettoussy-ab <abdollahettoussy@gmail.com>
2026-09-25 20:11:17 +0000

    3 commit

--- Projet.js
+++ Projet.js
@@ -1,6 +1,37 @@
 const prompt = require("prompt-sync")();
 
-const candidats=[];
+const candidats=[{
+cin : "AB123456",
+nom : "Boushaba",
+prenom : "Soufiane",
+partiPolitique : "Indépendant",
+age: 40,
+electeurs: ["HG1565135", "HG524565" , "HG663559" ,"L256455"]
+},
+{
+cin : "JE123456",
+nom : "JAWAD",
+prenom : "AMSA",
+partiPolitique : "VERSTAPEND",
+age: 17,
+electeurs: ["HH26582" , "HH255452" , "FR556523" ]
+},
+{
+cin : "HH123456",
+nom : "Mohmade",
+prenom : "bada",
+partiPolitique : "PAM",
+age: 30,
+electeurs: ["H142536" , "JU46895" ]
+},
+{
+cin : "AG543456",
+nom : "khalide",
+prenom : "waaaz",
+partiPolitique : "Nkhla",
+age: 32,
+electeurs: ["B506455"]
+}];
 
 let condition=true;
 while(condition){
@@ -62,8 +93,20 @@ while(condition){
 }
 
 function Ajouter_candidat (){
+    
+
+             
                 let newCin =prompt("CIN: ");
+
+                for(let i=0;i<candidats.length;i++){
+                    if(newCin === candidats[i].cin){
+                       console.log("Ce candidat est deja ajouté.");
+                       return;
+                    }
+                }
                 
+        
+        
                 let newNom =prompt("nom: ");
                 let newPrenom =prompt("prenom: ");
                 let newPartiPolitique =prompt("partiPolitique: ");
@@ -71,17 +114,20 @@ function Ajouter_candidat (){
                 
                 
                let newcondidat = {
-                      CIN: newCin ,
+                      cin: newCin ,
                       nom:newNom,
                       prenom:newPrenom,
                       partiPolitique:newPartiPolitique,
                       age:newAge,
-                      
+                      electeurs:[]
                  }
                  candidats.push(newcondidat);
                  console.log(candidats);
                  console.log("Le candidat a été ajouté avec succès.");
 }               
+                                                     
+
+            
 
 function Ajouter_plusieurs_candidats(){
     let nombreCondidat= parseInt(prompt("combien des condidat tu veux pour ajouiter?: "));
@@ -90,8 +136,26 @@ function Ajouter_plusieurs_candidats(){
    } 
 }
 
-function Afficher_la_list_des_candidats(){
+function Afficher_la_list_des_candidats()
+{
+     
+    for(let i = 0 ; i < candidats.length - 1; i++ )
+    {
+        for(let j = 0  ; j < candidats.length - 1 ; j++ )
+        {
+            if(candidats[j].electeurs.length < candidats[j+1].electeurs.length)
+            {
+                let swap = candidats[j];
+                candidats[j] = candidats[j+1];
+                candidats[j+1] = swap
+                break;
+            }
+                
 
+        }
+    }
+    
+console.table(candidats);
 }
 
 function Voter_pour_un_candidat(){
